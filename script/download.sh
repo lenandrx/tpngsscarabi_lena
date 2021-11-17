7f63ad741feef27f08d42041507806a0435e8e59
lena <lena.andrieux@ens-lyon.fr>
2021-11-17 13:59:17 +0000

    file to download the data of the wt and the mutant

---
+++ script/download.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# download the file for the wild type 3 and the mutant 5 
+
+cd ~/mydatalocal/tpngsscarabi_lena/
+echantillon=("SRR8257102 SRR8257104") 
+
+#parallel fastq dump permet de télécharger les données
+# découper en 8 threads pour diviser les tâches
+
+for i in $echantillon
+do
+echo $i
+parallel-fastq-dump --sra-id $i --threads 8 --outdir data --split-files --gzip
+done
+
