9bd2dff4dd6d09223442f65f4288946168c3768d
lena <lena.andrieux@ens-lyon.fr>
2021-11-18 13:16:17 +0000

    updated runalevin avec la création de la tg map

--- script/runalevin.sh
+++ script/runalevin.sh
@@ -5,7 +5,18 @@ cd ~/mydatalocal/tpngsscarabi_lena/
 # le document .fa contient la séquence 
 # le document .gtf contient toute l'information générale 
 
-wget -O data/arabidopsis_transcriptome.fa "https://ics.hutton.ac.uk/atRTD/RTD2/AtRTD2_19April2016.fa"
-wget -O data/arabidopsis_transcriptome.gtf "https://ics.hutton.ac.uk/atRTD/RTD2/AtRTD2_19April2016.gtf"
+#wget -O data/arabidopsis_transcriptome.fa "https://ics.hutton.ac.uk/atRTD/RTD2/AtRTD2_19April2016.fa"
+#wget -O data/arabidopsis_transcriptome.gtf "https://ics.hutton.ac.uk/atRTD/RTD2/AtRTD2_19April2016.gtf"
 
-salmon index -t data/arabidopsis_transcriptome.fa -p 8 -k 31 -i data_processed/
+# on crée le salmon index nécessaire pour run alevin ensuite
+
+# salmon index -t data/arabidopsis_transcriptome.fa -p 8 -k 31 -i data_processed/
+
+# il faut ensuite créer la tg map : un ficher de correspondance avec les transcrits 
+#on prend la neuvième colonne des lignes qui correspondent à des exons (première ligne)
+# on sélectionne les identifiants seulement avec les substr, sans les guillemets etc
+# on met ce qu'on obtient dans le fichier txp2gene qui se trouve dans le dossier data_processed
+
+bioawk -c gff '$feature=="exon" {print $9}' < data/arabidopsis_transcriptome.gtf) \
+  | awk -F ' ' '{print substr($2,2,length($2)-3) "\t" substr($4,2,length($2)-3)}' - > data_processed/txp2gene.tsv
+
