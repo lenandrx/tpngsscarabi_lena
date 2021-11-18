7667fb976d769fdc3136d3bbbca1f68865cc2438
lena <lena.andrieux@ens-lyon.fr>
2021-11-18 16:19:15 +0000

    updated alevin script to run alevin for mutant and wt

--- script/alevin.sh
+++ script/alevin.sh
@@ -4,8 +4,8 @@ cd ~/mydatalocal/tpngsscarabi_lena/
 # il faut maintenant faire alevin, une fois pour le wt et une fois pour le mutant 
 salmon alevin -l ISR -1 data/SRR8257102_1.fastq.gz -2 data/SRR8257102_2.fastq.gz \
   --chromium -i data_processed/index_salmon/ -p 8 -o results/results_alevin_wt/ \
-  --tgMap data_processed/txp2gene.tsv  
+  --tgMap data_processed/txp2gene.tsv --dumpFeatures
 
 salmon alevin -l ISR -1 data/SRR8257104_1.fastq.gz -2 data/SRR8257104_2.fastq.gz \
   --chromium -i data_processed/index_salmon/ -p 8 -o results/results_alevin_mutant/ \
-  --tgMap data_processed/txp2gene.tsv  
+  --tgMap data_processed/txp2gene.tsv --dumpFeatures
