3030de2d04f2a0d3bcfe61d65fb0985a2f2af597
lena <lena.andrieux@ens-lyon.fr>
2021-11-18 10:30:03 +0000

    nouveau script pour le programme alevin _ import du transcriptome dans le dossier data

---
+++ script/runalevin.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd ~/mydatalocal/tpngsscarabi_lena/
+
+# on récupère le transcriptome d'arabidopsis et on le met dans le fichier data
+
+wget -o data/"https://ics.hutton.ac.uk/atRTD/RTD2/AtRTD2_19April2016.fa"
