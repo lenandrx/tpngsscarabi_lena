cbdd01f000452ebb9c0d4969c652291fd621e350
lena <lena.andrieux@ens-lyon.fr>
2021-12-14 10:07:34 +0000

    Création d'un script markdown R avec l'analyse Seurat

---
+++ script/import_donneestotales.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#script deuxième semaine : on importe les données de tous les échantillons
+
+cd ~/mydatalocal/tpngsscarabi_lena/data/
+
+#les quatre premiers sont des WT, les trois suivants sont des mutants 
+
+wget -r --user=tp_ngs --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/scarabi/Alevin/quant
+
+# parmi ces données on n'a gardé que le quant, et on a supprimé le reste 
