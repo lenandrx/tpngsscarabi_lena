ba0663ddf56d020c8924b9a8b0b99193340e4bc1
lena <lena.andrieux@ens-lyon.fr>
2021-12-14 15:22:35 +0000

    script pour importer le fichier contenant les marqueurs et types cellulaires

---
+++ script/data_markers.sh
@@ -0,0 +1,8 @@
+#! /bin/bash 
+
+Importer le fichier csv des associations de marqueurs de gènes pour chaque type cellulaire 
+
+cd ~/mydatalocal/tpngsscarabi_lena/data/
+
+wget --user=tp_ngs --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/Markers.csv
+
