f81eaf562acf74024841c9d3a478453daf0d9122
lena <lena.andrieux@ens-lyon.fr>
2021-12-15 08:57:47 +0000

    ajout de l'import des données brutes par type cellulaire après dissection

--- script/data_markers.sh
+++ script/data_markers.sh
@@ -1,8 +1,12 @@
 #! /bin/bash 
 
-Importer le fichier csv des associations de marqueurs de gènes pour chaque type cellulaire 
+#Importer le fichier csv des associations de marqueurs de gènes pour chaque type cellulaire 
 
-cd ~/mydatalocal/tpngsscarabi_lena/data/
+cd ~/mydatalocal/tpngsscarabi_lena/data
 
 wget --user=tp_ngs --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/Markers.csv
+
+#On importe les données brutes obtenues par dissection de chaque type cellulaire 
+
+wget -r --no-parent --user=tp_ngs --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/Salmon/Counts_Salmon/ 
 
